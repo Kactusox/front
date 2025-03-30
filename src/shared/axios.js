db910a0c462d25920c7f36638383100f797eea8a
Kactusox <joldasbaevkhan00@gmail.com>
2025-03-30 15:04:11 +0500

    updated api link

--- src/shared/axios.js
+++ src/shared/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { CAR_COMPARISON_TOKEN } from "./types";
 
-export const BASE_URL = "http://localhost:8080";
+export const BASE_URL = "https://backend-production-88d3.up.railway.app";
 export const TOKEN = localStorage.getItem(CAR_COMPARISON_TOKEN);
 
 const httpClient = axios.create({
